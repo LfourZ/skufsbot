c81c4741c87bcd2c73cb3071e76884098ed08aa7
lfourunderscore <lfourunderscore@gmail.com>
2016-11-02 16:51:45 +0200

    Switched from XML to JSON

--- libs/perm.lua
+++ libs/perm.lua
@@ -1,5 +1,10 @@
 local M = {}
 
+--A few requires
+local ltable = require("./ltable") --For a few table functions
+local httpfunctions = require("./httpFunctions") --For http functions and xml to table
+local json = require("./json")
+
 --Users, roles and channels that get instant thumbs up for new commands
 local ROOT_ROLE = "none"
 local ROOT_USER = "184262286058323968"
@@ -18,8 +23,12 @@ _G.defcommands["playerlist"] = {}
 _G.defcommands["test"] = {}
 _G.defcommands["permission"] = {}
 _G.defcommands["whocanuse"] = {}
-_G.defcommands["editelement"] = {}
 _G.defcommands["printelement"] = {}
+
+local function ldebug(String)
+	if not DEBUG then return end
+	print(string.format("[%s]: %s", os.date("%c"), String))
+end
 
 --Prints data found in path Message seperated by whitespaces.
 local function printElement(String, Message)
@@ -44,22 +53,19 @@ local function printElement(String, Message)
 end
 M.printElement = printElement
 
-
-
---A few requires
-local xml = require("./tableToXml") --For table to xml
-local ltable = require("./ltable") --For a few table functions
-local httpfunctions = require("./httpFunctions") --For http functions and xml to table
-
---
-local function ldebug(String)
-	if not DEBUG then return end
-	print(string.format("[%s]: %s", os.date("%c"), String))
+--Initial test to use JSON
+local function savePermFile(Server)
+	ldebug("Running function "..debug.getinfo(1, "n").name)
+	local file = io.open("./perms/perms_"..Server.id..".json", "w")
+	io.output(file)
+	io.write(json.encode(_G.perms.servers[Server.id], { indent = true }))
+	io.close(file)
 end
+M.savePermFile = savePermFile
 
 --Checks if Char is one of the server's silent or loud character. This determines whether the command gets deleted
 local function isSilent(Char, Server)
-	ldebug(debug.getinfo(1, "n").name)
+	ldebug("Running function "..debug.getinfo(1, "n").name)
 	if Char == _G.perms.servers[Server.id].data.silentchar then
 		return true
 	elseif Char == _G.perms.servers[Server.id].data.loudchar then
@@ -72,50 +78,34 @@ M.isSilent = isSilent
 
 --Checks Table for Id, if nothing is found, it defaults to Bool
 local function checkForPerm(Table, Id, Bool) --DONE
-	if DEBUG then print(debug.getinfo (1, "n").name) end
+	ldebug("Running function "..debug.getinfo(1, "n").name)
 	local result = false
-	if Table[Id] == "true" then
+	if Table[Id] == true then
 		result = true
-	elseif Table[Id] == "false" then
+	elseif Table[Id] == false then
 		result = false
 	else
-		if DEBUG then 
-			if Table[Id] == nil then
-				print("ERROR: Unexpected perm result: nil. Defaulting to "..tostring(Bool))
-			end
+		if Table[Id] == nil then
+			ldebug("ERROR: Unexpected perm result: nil. Defaulting to "..tostring(Bool))
 		end
 		result = Bool
 	end
-	if result and DEBUG then
-		print("^ allowed")
-	else
-		print("^ not allowed")
-	end
+	ldebug("Function "..debug.getinfo(1, "n").name.." returned: "..tostring(result))
 	return result
 end
 M.defaultPerm = defaultPerm
 
 --Function to check if file exists, used in other functions
 local function fileExists(Name) --DONE
-	if DEBUG then print(debug.getinfo (1, "n").name) end
+	ldebug("Running function "..debug.getinfo(1, "n").name)
    	local f=io.open(Name,"r")
    	if f~=nil then io.close(f) return true else return false end
 end
 M.fileExists = fileExists
 
---Overwrites XML file with permissions with whatever is currently in the permissions table
-local function savePermFile(Server)
-	if DEBUG then print(debug.getinfo (1, "n").name) end
-	local file = io.open("./perms/perms_"..Server.id..".xml", "w")
-	io.output(file)
-	io.write(xml.tableToXml(_G.perms.servers[Server.id]))
-	io.close(file)
-end
-M.savePermFile = savePermFile
-
 --Creates table structure and default entries for a server, used for instance when new server is detected
 local function generatePermTable(Server, Table) --I DUNNO
-	if DEBUG then print(debug.getinfo (1, "n").name) end
+	ldebug("Running function "..debug.getinfo(1, "n").name)
 	local tbl = {}
 	tbl["data"] = {}
 	tbl.data["silentchar"] = "/"
@@ -123,7 +113,7 @@ local function generatePermTable(Server, Table) --I DUNNO
 	tbl.data["servername"] = Server.name
 	tbl.data["joinmsg"] = "User has joined the server"
 	tbl.data["leavemsg"] = "User has left the server"
-	tbl.data["msgchannel"] = Server.defaultChannel
+	tbl.data["msgchannel"] = Server.defaultChannel.id
 	tbl.data["msgsettings"] = "0"
 	tbl["commands"] = {}
 	for k, v in pairs(Table) do
@@ -142,7 +132,7 @@ M.generatePermTable = generatePermTable
 
 --Registers a new Command on a Server. Called when default command doesn't exist on a server
 local function addCommand(Command, Server)
-	if DEBUG then print(debug.getinfo (1, "n").name) end
+	ldebug("Running function "..debug.getinfo(1, "n").name)
 	_G.perms.servers[Server.id].commands[Command] = {}
 	_G.perms.servers[Server.id].commands[Command].roles =  {}
 	_G.perms.servers[Server.id].commands[Command].roles[ROOT_ROLE] = true
@@ -154,31 +144,36 @@ local function addCommand(Command, Server)
 end
 M.addCommand = addCommand
 
---Converts XML with permissions for Server into table and loads it in the global table
+--Converts JSON with permissions for Server into table and loads it in the global table
 local function loadPermFile(Server)
-	if DEBUG then print(debug.getinfo (1, "n").name) end
-	local file = io.open("./perms/perms_"..Server.id..".xml", "r")
+
+	ldebug("Running function "..debug.getinfo(1, "n").name)
+	local file = io.open("./perms/perms_"..Server.id..".json", "r")
 	io.input(file)
-	local tbl = httpfunctions.xmlToTable(io.read("*a"))
-	_G.perms.servers[Server.id] = tbl.root
+	local worked = false
+
+	local obj, pos, err = json.decode(io.read("*a"), 1, nil)
+	if err then
+  		ldebug("Error:", err)
+	else
+		_G.perms.servers[Server.id] = obj
+		worked = true
+	end
 	io.close(file)
-	return true
+	return worked
 end
 M.loadPermFile = loadPermFile
 
 --Checks if Command exists on Server. If not, checks if Command is a default command, and if so, loads it
 local function commandExists(Command, Server)
-	if DEBUG then print(debug.getinfo (1, "n").name) end
+	ldebug("Running function "..debug.getinfo(1, "n").name)
 	if _G.perms.servers[Server.id].commands[Command] ~= nil then
-		if DEBUG then print("True") end
 		return true
 	else
 		if _G.defcommands[Command] ~= nil then
 			addCommand(Command, Server)
-			if DEBUG then print("True") end
 			return true
 		else
-			if DEBUG then print("False") end
 			return false
 		end
 	end
@@ -187,9 +182,9 @@ M.commandExists = commandExists
 
 --Checks for permission XML file for ALL servers, when server without file is found, generates server file with default commands
 local function checkForPermFile(Client) --ALSO DONE
-	if DEBUG then print(debug.getinfo (1, "n").name) end
+	ldebug("Running function "..debug.getinfo(1, "n").name)
 	for k, v in pairs(Client.servers) do
-		if not fileExists("./perms/perms_"..v.id..".xml") then
+		if not fileExists("./perms/perms_"..v.id..".json") then
 			generatePermTable(v, _G.defcommands)
 		else
 			loadPermFile(v)
@@ -200,7 +195,7 @@ M.checkForPermFile = checkForPermFile
 
 --Checks if the permissions are loaded in the global table for Server
 local function isPermsLoaded(Server) --DONE
-	if DEBUG then print(debug.getinfo (1, "n").name) end
+	ldebug("Running function "..debug.getinfo(1, "n").name)
 	if _G.perms.servers[Server.id] ~= nil then
 		return true
 	else
@@ -213,7 +208,7 @@ M.isPermsLoaded = isPermsLoaded
 --returns true or false (if found), and breaks. If values don't match, goes back to beginning. If nothing is found,
 --return defaults to Bool
 local function crossCheckKey(Table, Table2, Bool, Server)
-	if DEBUG then print(debug.getinfo (1, "n").name) end
+	ldebug("Running function "..debug.getinfo(1, "n").name)
 	local found = false
 	local result = Bool
 	for k, v in pairs(Table) do
@@ -229,7 +224,7 @@ local function crossCheckKey(Table, Table2, Bool, Server)
 					result = false
 					break
 				else
-					if DEBUG then print("ERROR: Unexpected perm value: "..vr) end
+					ldebug("ERROR: Unexpected perm value: "..vr)
 					found = true
 				end
 			end
@@ -251,7 +246,7 @@ M.crossCheckKey = crossCheckKey
 
 --Crosschecks the elements of two tables, returning true and ending when exact matches are found
 local function crossCheckElement(Table, Table2) --READ ABOVE
-	if DEBUG then print(debug.getinfo (1, "n").name) end
+	ldebug("Running function "..debug.getinfo(1, "n").name)
 	local found = false
 	for k, v in pairs(Table) do
 		if found then break end
@@ -287,8 +282,7 @@ M.channelAllowed = channelAllowed
 --Combines the three above functions to get an absolute value. role and channel have to be true, and user, if
 --existant, overrides everything. This means that user permissions are for fine tuning.
 local function canUse(Message, Command)
-	print("canuse")
-	ldebug(debug.getinfo(1, "n").name)
+	ldebug("Running function "..debug.getinfo(1, "n").name)
 	local response = false
 	if roleAllowed(Message.author, Command) then
 		if channelAllowed(Message, Command) then
@@ -305,7 +299,7 @@ M.canUse = canUse
 --Changes permissions for everyone in the Table of mentions, depending on the Args. Returns string 'str' with
 --info about the permission changes, formatted with backticks for discord
 local function editPerms(Table, Args, Server)
-	if DEBUG then print(debug.getinfo (1, "n").name) end
+	ldebug("Running function "..debug.getinfo(1, "n").name)
 	local action, commande = string.match(Args, "(%S+) (%S+)")
 	if not commandExists(commande, Server) then print("Command "..commande.." doesn't exist.") return end
 	local str = "```\n"
@@ -356,7 +350,7 @@ local function whoCanUse(Command, Server, Client)
 			if Client:getMemberById(k) ~= nil then
 				str = str..Client:getMemberById(k).name.." ("..k..") "..allowed.."\n"
 			else
-				if DEBUG then print("Invalid user ID found in "..Server.name) end
+				ldebug("Invalid user ID found in "..Server.name)
 			end
 		end
 	end
@@ -373,7 +367,7 @@ local function whoCanUse(Command, Server, Client)
 			if Client:getChannelById(k) ~= nil then
 				str = str..Client:getChannelById(k).name.." ("..k..") "..allowed.."\n"
 			else
-				if DEBUG then print("Invalid channel ID found in "..Server.name) end
+				ldebug("Invalid channel ID found in "..Server.name)
 			end
 		end
 	end
@@ -390,12 +384,11 @@ local function whoCanUse(Command, Server, Client)
 			if Client:getRoleById(k) ~= nil then
 				str = str..Client:getRoleById(k).name.." ("..k..") "..allowed.."\n"
 			else
-				if DEBUG then print("Invalid role ID found in "..Server.name) end
+				ldebug("Invalid role ID found in "..Server.name)
 			end
 		end
 	end
 	str = str.."```"
-	if DEBUG then print(str) end
 	return str
 end
 M.whoCanUse = whoCanUse
